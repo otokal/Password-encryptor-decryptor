eca12d64e5ab057d9a3fcb46cc05d8331628fd8f
otokal <74141725+otokal@users.noreply.github.com>
2023-12-06 18:47:40 +0100

    Add files via upload
    This is the code for the password encryption and decryption.

---
+++ CRYPTOR.py
@@ -0,0 +1,112 @@
+import tkinter as tk
+from cryptography.fernet import Fernet
+
+class MyWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # Set the title of the window
+        self.title("Password Locker")
+
+        # Set the dimensions of the window
+        self.geometry("400x300")
+
+        # Create a frame to hold the buttons and label
+        self.frame = tk.Frame(self)
+        self.frame.pack(expand=True)
+
+        # Create the first button
+        self.button1 = tk.Button(self.frame, text="ENCRYPT", width=10, command=self.open_window1)
+        self.button1.grid(row=0, column=0, pady=10)
+
+        # Create the "OR" label
+        self.label_or = tk.Label(self.frame, text="OR")
+        self.label_or.grid(row=1, column=0, pady=10)
+
+        # Create the second button
+        self.button2 = tk.Button(self.frame, text="DECRYPT", width=10, command=self.open_window2)
+        self.button2.grid(row=2, column=0, pady=10)
+
+        # Center the frame vertically and horizontally
+        self.update()
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    # Function to open Window 1
+    def open_window1(self):
+        self.window1 = tk.Toplevel(self)
+        self.window1.title("ENCRYPTOR")
+        self.window1.geometry("400x300")
+
+        # Create Generate Key button and text box
+        generate_key_button = tk.Button(self.window1, text="Generate Key", width=15, command=self.generate_key)
+        generate_key_button.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.key_entry = tk.Entry(self.window1, width=30)
+        self.key_entry.grid(row=0, column=1, padx=10, pady=10, sticky=tk.E)
+
+        # Create Encrypt button, password text box, and encrypted password text box
+        encrypt_button = tk.Button(self.window1, text="Encrypt", width=15, command=self.encrypt_password)
+        encrypt_button.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.password_entry = tk.Entry(self.window1, width=30)
+        self.password_entry.grid(row=1, column=1, padx=10, pady=10, sticky=tk.E)
+
+        self.encrypted_password_entry = tk.Entry(self.window1, width=30)
+        self.encrypted_password_entry.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+    def generate_key(self):
+        key = Fernet.generate_key()
+        self.key_entry.delete(0, tk.END)
+        self.key_entry.insert(tk.END, key.decode())
+
+    def encrypt_password(self):
+        password = self.password_entry.get()
+        key = self.key_entry.get()
+        f = Fernet(key.encode())
+        encrypted_password = f.encrypt(password.encode())
+        self.encrypted_password_entry.delete(0, tk.END)
+        self.encrypted_password_entry.insert(tk.END, encrypted_password.decode())
+
+    # Function to open Window 2
+    def open_window2(self):
+        self.window2 = tk.Toplevel(self)
+        self.window2.title("DECRYPTOR")
+        self.window2.geometry("400x300")
+
+        # Create Enter Secret Key text box and Encrypted Password text box
+        enter_key_label = tk.Label(self.window2, text="Enter Secret Key:")
+        enter_key_label.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.secret_key_entry = tk.Entry(self.window2, width=30)
+        self.secret_key_entry.grid(row=0, column=1, padx=10, pady=10)
+
+        encrypted_password_label = tk.Label(self.window2, text="Encrypted Password:")
+        encrypted_password_label.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.encrypted_password_entry2 = tk.Entry(self.window2, width=30)
+        self.encrypted_password_entry2.grid(row=1, column=1, padx=10, pady=10)
+
+        # Create Decrypt button and Decrypted Password text box
+        decrypt_button = tk.Button(self.window2, text="Decrypt", width=15, command=self.decrypt_password)
+        decrypt_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+        decrypted_password_label = tk.Label(self.window2, text="Decrypted Password:")
+        decrypted_password_label.grid(row=3, column=0, padx=10, pady=10, sticky=tk.W)
+
+        self.decrypted_password_entry = tk.Entry
+        self.decrypted_password_entry = tk.Entry(self.window2, width=30)
+        self.decrypted_password_entry.grid(row=3, column=1, padx=10, pady=10)
+
+    def decrypt_password(self):
+        encrypted_password = self.encrypted_password_entry2.get()
+        key = self.secret_key_entry.get()
+        f = Fernet(key.encode())
+        decrypted_password = f.decrypt(encrypted_password.encode())
+        self.decrypted_password_entry.delete(0, tk.END)
+        self.decrypted_password_entry.insert(tk.END, decrypted_password.decode())
+
+# Create an instance of the custom MyWindow class
+window = MyWindow()
+
+# Start the main event loop to display the window
+window.mainloop()
